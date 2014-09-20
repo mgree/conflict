454c80ada160889d28e013102163848adc15bd0f
Michael Greenberg <michael.m.greenberg@gmail.com>
2014-09-20 12:02:42 -0400

    greedy search

--- ordering.hs
+++ ordering.hs
@@ -8,6 +8,8 @@ import Control.Monad.Random
 
 import Text.CSV
  
+import Data.Ord
+import Data.Function (on)
 import Data.List
 import qualified Data.Set as Set
 import qualified Data.Map as Map
@@ -24,6 +26,38 @@ diff_select n xs = do
 
 permute :: RandomGen g => [a] -> Rand g [a]
 permute xs = diff_select (length xs) xs
+
+type Paper = String
+type Email = String
+
+type Conflicts = Map.Map Paper [Email]
+
+with :: Conflicts -> Paper -> [Email]
+with c p = sort $ Map.findWithDefault [] p c
+
+overlap :: [String] -> [String] -> Int
+overlap xs ys = length $ xs `intersect` ys
+
+bestChoice :: Paper -> [Paper] -> Conflicts -> (Paper,[Paper])
+bestChoice paper papers conflicts =
+  let conflicted = conflicts `with` paper in
+  let options = map (\p -> (p,Down $ conflicted `overlap` (conflicts `with` p))) papers in
+  let (best:rest) = map fst $ sortBy (compare `on` snd) options in
+  (best,rest)
+  
+greedySearch :: Paper -> [Paper] -> Conflicts -> [Paper]
+greedySearch paper [] _ = [paper]
+greedySearch paper papers conflicts =
+  let (next,papers') = bestChoice paper papers conflicts in
+  paper : greedySearch next papers' conflicts
+
+greedy :: RandomGen g => [Paper] -> Conflicts -> Rand g [Paper]
+greedy papers conflicts = do
+  let n = length papers
+  r <- getRandomR (0,n-1) 
+  let pivot = papers !! r
+  let papers' = take r papers ++ drop (r+1) papers
+  return $ greedySearch pivot papers' conflicts
 
 format :: Show a => [a] -> String
 format [] = ""
@@ -76,13 +110,13 @@ main = do
                                    paper `elem` papers &&
                                    email `Set.member` emails) 
                           rawConflicts
-      let conflicts = Map.fromListWith (++) $ 
+      let conflicts = Map.fromListWith (\cs1 cs2 -> sort $ cs1 ++ cs2) $ 
                       map (\[paper,_,email,_] -> (paper,[email])) realConflicts
       
       -- reorder the papers
-      ordering <- evalRandIO $ permute papers
+      ordering <- evalRandIO $ greedy papers conflicts
       
-      let schedule = map (\paper -> paper : (Map.findWithDefault [] paper conflicts)) 
+      let schedule = map (\paper -> paper : (conflicts `with` paper)) 
                      ordering
 
       putStrLn $ intercalate "\n" $ map (intercalate ",") schedule
